e37821784654e787467b588b20c189698573fbd4
freddeP <persson.fredric@gmail.com>
2024-10-17 09:32:35 +0200

    lektion 17 okt

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const exp = require('constants');
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require("fs");
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// FIXA så att vi har tillgång till bl a CSS
+app.use(express.static("public"))
+// FIXA EN BODY till POST
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/', (req, res) => res.send(render('Hello World!')))
 
 // Route för att gå till ett registreringsformulär
 app.get("/register", showRegister);
@@ -11,6 +20,39 @@ app.get("/register", showRegister);
 // Route för att gå till ett loginformulär
 app.get("/login", showLogin);
 
+// Route för att HANTERA registrering
+app.post("/register", register);
 
-function showRegister(req, res){}
-function showLogin(req, res){}
+
+function register(req, res){
+
+    let data = req.body;
+    res.send(render(JSON.stringify(data)))
+
+}
+
+
+
+function showRegister(req, res){
+
+
+
+    let form = fs.readFileSync("html/register.html").toString();
+    res.send(render(form));
+
+}
+function showLogin(req, res){
+    let form = fs.readFileSync("html/login.html").toString();
+    res.send(render(form));
+}
+
+
+
+
+function render(content){
+
+    let html = require("fs").readFileSync("html/template.html").toString();
+    return html.replace("{content}", content);
+
+
+}
